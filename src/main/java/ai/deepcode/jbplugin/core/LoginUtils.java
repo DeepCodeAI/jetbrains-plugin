ba9e80d5f77211c4dc1c3859bc00c7d4c53d794f
Artsiom Chapialiou <achapialiou@gmail.com>
2020-05-23 14:28:10 +0300

    tests for login process added

--- src/main/java/ai/deepcode/jbplugin/core/LoginUtils.java
+++ src/main/java/ai/deepcode/jbplugin/core/LoginUtils.java
@@ -54,15 +54,17 @@ public class LoginUtils {
   }
 
   /** network request! */
-  public static void requestNewLogin(@NotNull Project project) {
+  public static void requestNewLogin(@NotNull Project project, boolean openBrowser) {
     DCLogger.info("New Login requested.");
     DeepCodeParams.clearLoginParams();
     LoginResponse response = DeepCodeRestApi.newLogin(userAgent);
     if (response.getStatusCode() == 200) {
-    DCLogger.info("New Login request succeed. New Token: " + response.getSessionToken());
+      DCLogger.info("New Login request succeed. New Token: " + response.getSessionToken());
       DeepCodeParams.setSessionToken(response.getSessionToken());
       DeepCodeParams.setLoginUrl(response.getLoginURL());
-      BrowserUtil.open(DeepCodeParams.getLoginUrl());
+      if (openBrowser) {
+        BrowserUtil.open(DeepCodeParams.getLoginUrl());
+      }
       if (!isLoginCheckLoopStarted) {
         ReadAction.nonBlocking(() -> startLoginCheckLoop(project))
             .submit(NonUrgentExecutor.getInstance());
---
+++ src/test/java/ai/deepcode/jbplugin/TestDCPluginCodeInsightTest.java
@@ -0,0 +1,53 @@
+package ai.deepcode.jbplugin;
+
+import ai.deepcode.jbplugin.core.DeepCodeParams;
+import ai.deepcode.jbplugin.core.LoginUtils;
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+
+/**
+ * See: https://www.jetbrains.org/intellij/sdk/docs/basics/testing_plugins/testing_plugins.html See:
+ * https://www.jetbrains.org/intellij/sdk/docs/tutorials/writing_tests_for_plugins.html
+ */
+public class TestDCPluginCodeInsightTest extends BasePlatformTestCase {
+
+  // !!! Will works only with already logged sessionToken
+  private static final String loggedToken =
+      "aeedc7d1c2656ea4b0adb1e215999f588b457cedf415c832a0209c9429c7636e";
+
+  @Override
+  protected String getTestDataPath() {
+    return "src/test/testData";
+  }
+
+  public void testLogin() {
+    final Project project = myFixture.getProject();
+
+    DeepCodeParams.setSessionToken("blablabla");
+    assertFalse("Login with malformed Token should fail", LoginUtils.isLogged(project, false));
+
+    LoginUtils.requestNewLogin(project, false);
+    assertFalse(
+        "Login with newly requested but not yet logged token should fail",
+        LoginUtils.isLogged(project, false));
+
+    DeepCodeParams.setSessionToken(loggedToken);
+    PropertiesComponent.getInstance(project).setValue("consentGiven", false);
+    assertFalse("Login without Consent should fail", LoginUtils.isLogged(project, false));
+
+    DeepCodeParams.setSessionToken(loggedToken);
+    DeepCodeParams.setConsentGiven(project);
+    assertTrue(
+        "Login with logged Token and confirmed Consent should pass",
+        LoginUtils.isLogged(project, false));
+  }
+
+  public void testAnnotator() {
+    DeepCodeParams.setSessionToken(loggedToken);
+    DeepCodeParams.setConsentGiven(myFixture.getProject());
+    myFixture.configureByFile("AnnotatorTest1.js");
+    myFixture.checkHighlighting(true, true, true, true);
+  }
+}
