cdbf5f7b6a1e68c6003de09c42c6aac06c9224e4
Artsiom Chapialiou <achapialiou@gmail.com>
2020-05-25 16:15:27 +0300

    fix deadlocks when files rapidly changes "from outside"

--- src/test/java/ai/deepcode/jbplugin/TestAnnotatorForJava.java
+++ src/test/java/ai/deepcode/jbplugin/TestAnnotatorForJava.java
@@ -6,16 +6,12 @@ import ai.deepcode.jbplugin.core.RunUtils;
 public class TestAnnotatorForJava extends MyBasePlatformTestCase {
 
   public void testHighlighting_Java() {
-    return;
-    // fixme: order dependant for some reason...
-/*
     DeepCodeParams.setSessionToken(loggedToken);
     DeepCodeParams.setConsentGiven(project);
     myFixture.configureByFile("AnnotatorTest.java");
     //fixme: delay to let annotators do the job
     RunUtils.delay(2000);
     myFixture.checkHighlighting(true, true, true, true);
-*/
   }
 
 }
